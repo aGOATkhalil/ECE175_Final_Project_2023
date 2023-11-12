68b0c6be7609feb2a9c1eff52f0f5536074b5f8b
Khalil <kdgatto01@gmail.com>
2023-11-12 13:34:15 -0700

    added player name

--- Main Code/main.c
+++ Main Code/main.c
@@ -7,8 +7,14 @@
 ***************************************************************************************************************
 */
 
+
+#define CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 
 typedef struct card_s			//can add or change to this but not delete any preexisting
 {
@@ -17,15 +23,26 @@ typedef struct card_s			//can add or change to this but not delete any preexisti
     struct card_s* next;
 } card;
 
-Shuffle_Card()			//"should work with any size deck of cards"
-{						//"[see sec 2.24 Random numbers in your zyBooks]"
-            //"You should seed the random number generator with a call to time() with srand()"
+Shuffle_Card()			
+{				
+   //"should work with any size deck of cards"
+   //"[see sec 2.24 Random numbers in your zyBooks]"
+   //"You should seed the random number generator with a call to time() with srand()"
+
+
+
 }
 
 int main(void) {
 
     //blah blah stuff blah blah
     //balls
+
+    char player_name[100]; //may change later. names may not always be just 100 char long.
+
+    printf("Enter your name: ");
+    scanf("%s", player_name);
+    printf("\n%s lets play go fish!\n", player_name);
 
 
 
