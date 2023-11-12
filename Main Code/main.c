90a86db386666b058557135c42aab888c7f0d9f8
Khalil <kdgatto01@gmail.com>
2023-11-12 13:14:53 -0700

    added main from replit

---
+++ Main Code/main.c
@@ -0,0 +1,30 @@
+//Rayce Bacchus & Khalil Gatto
+//11-4-2023
+
+/*
+***************************************************************************************************************
+                    USE LINKED LISTS OR ELSE GET 0
+***************************************************************************************************************
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct card_s			//can add or change to this but not delete any preexisting
+{
+    char suit;
+    int face;
+    struct card_s* next;
+} card;
+
+Shuffle_Card()			//"should work with any size deck of cards"
+{						//"[see sec 2.24 Random numbers in your zyBooks]"
+            //"You should seed the random number generator with a call to time() with srand()"
+}
+
+int main(void) {
+
+    //blah blah stuff blah blah
+
+    return 0;
+}
