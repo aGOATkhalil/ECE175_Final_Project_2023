ac389a2c30fe8db9c69346ffa6099f540595523e
Khalil <kdgatto01@gmail.com>
2023-11-14 22:06:34 -0700

    added cmake.

---
+++ CMakeProject1/CMakeProject1.c
@@ -0,0 +1,56 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+int main(void)
+{
+    //Rayce Bacchus & Khalil Gatto
+    //11-4-2023
+
+    /*
+    ***************************************************************************************************************
+                        USE LINKED LISTS OR ELSE GET 0
+    ***************************************************************************************************************
+    */
+
+
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+    typedef struct card_s			//can add or change to this but not delete any preexisting
+    {
+        char suit;
+        int face;
+        struct card_s* next;
+    } card;
+
+    void Shuffle_Card(); {
+
+        printf("hi");
+        //"should work with any size deck of cards"
+        //"[see sec 2.24 Random numbers in your zyBooks]"
+        //"You should seed the random number generator with a call to time() with srand()"
+    }
+
+    int main(void); {
+
+        //blah blah stuff blah blah
+        //balls
+
+        char player_name[100]; //may change later. names may not always be just 100 char long.
+
+        printf("Enter your name: ");
+        scanf("%s", player_name);
+        printf("\n%s lets play go fish!\n", player_name);
+
+
+
+        return 0;
+    }
+
+}
---
+++ CMakeProject1/CMakeProject1.h
@@ -0,0 +1,11 @@
+﻿// CMakeProject1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+// TODO: Reference additional headers your program requires here.
